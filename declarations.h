e5e971a610245ec97d9e47e34376421aec3773d1
Santiago <spinillac@unal.edu.co>
2022-11-10 14:50:54 -0500

    Se agrego clase vector3d

--- declarations.h
+++ declarations.h
@@ -4,12 +4,11 @@
 
 //Constante G
 const double G = 1;
-const int NSTEPS = 100000; //Pasos
-const double DT = 0.1; //Intervalo de tiempo
+const double T = 62.80; //Tiempo
+const double DT = 0.01; //Intervalo de tiempo
+const int NSTEPS = int(T/DT); //Pasos
 
-//
-void initial_conditions(Planeta & body1, Planeta & body2);
+
+// function declaratons
 void force(Planeta & body1, Planeta & body2);
-void start_leap_frog(Planeta & body, const double & dt);
-void leap_frog(Planeta & body, const double & dt);
 void print(Planeta & body1, Planeta & body2, double time);
