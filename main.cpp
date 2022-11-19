8dea7b9992c0cb2603186d08ce7a7b2f4f52a20f
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-19 11:34:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include "declarations.h"
 
 int main(){
-    double ms = 333000.0;
-
-    //Datos tierra
+    //Datos Sol
+    double mS = 333000.0;
+    //Datos Tierra
     double mt = 1.0;
     double rt = 1.0;
-    double v0t = sqrt(G*ms*pow(rt, -3))*rt;
-    //
+    double v0t = sqrt(G*mS*pow(rt, -3))*rt;
+    //Datos Jupiter
     double mj = 317.81;
     double rj = 5.205;
-    double v0j = sqrt(G*ms*pow(rj, -3))*rj;
+    double v0j = sqrt(G*mS*pow(rj, -3))*rj;
+    //Datos Sonda
+    double ms = 4.225E-22;
+    double v0s = 0.7;
+    double r0s = rt + (4.286E-5);
 
     //---(x0,y0,z0,Vx0,Vy0,Vz0,m)
-    Cuerpo Sol(0,0,0,0,0,0,ms);
+    Cuerpo Sol(0,0,0,0,0,0,mS);
     Cuerpo Tierra(rt,0,0,0,v0t,0,mt);
     Cuerpo Jupiter(rj,0,0,0,v0j,0,mj);
-    Cuerpo Sonda(50.2,0,0,0,20*sqrt(6)/3,0, 0.0001);
+    Cuerpo Sonda(r0s,0,0,0,v0s,0, ms);
 
-    cuerpos N = {Sol, Jupiter, Tierra};
+    cuerpos N = {Sonda, Sol, Jupiter, Tierra};
     //Fuerza inicial
-    uptade(N);
+    force(N);
     Sol.start_leap_frog(DT);
     Tierra.start_leap_frog(DT);
     Jupiter.start_leap_frog(DT);
@@ -29,10 +33,7 @@ int main(){
     print(N);
 
     for(int ii = 0; ii < NSTEPS; ++ii){
-        uptade(N);
-        // Sol.leap_frog(DT);
-        // Tierra.leap_frog(DT);
-        // Jupiter.leap_frog(DT);
+        force(N);
         trans_galileo(N);
         print(N);
     }
