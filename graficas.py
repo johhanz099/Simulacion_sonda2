0c8a9633380f98c60e49b2b5f4e7ebfddeff3bcf
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-20 20:18:01 -0500

    Update graficas.py

--- graficas.py
+++ graficas.py
@@ -4,20 +4,23 @@ import matplotlib.pylab as plt
 data = np.genfromtxt("data.txt", delimiter = ",")
 
 #t =  data[:,0]
-sx = data[:,4]
-sy = data[:,5]
-tx = data[:,0]
-ty = data[:,1]
-jx = data[:,2]
-jy = data[:,3]
-#sondax = data[:,4]
-#sonday = data[:,5]
+sx = data[:,6]
+sy = data[:,7]
+tx = data[:,2]
+ty = data[:,3]
+jx = data[:,4]
+jy = data[:,5]
+sondax = data[:,0]
+sonday = data[:,1]
+
+print("Maximo sonda: ", sondax.min())
+print("Maximo sonda: ", jx.min())
 
 plt.figure()
 plt.plot(sx,sy,label="Sol y(x)")
 plt.plot(tx,ty,label="Tierra y(x)")
-plt.plot(jx,jy,label="Marte y(x)")
-# plt.plot(sondax,sonday,label="Sonda y(x)")
+plt.plot(jx,jy,label="Jupiter y(x)")
+plt.plot(sondax,sonday,label="Sonda y(x)")
 plt.legend()
 plt.savefig("Sol_Tierra_yx.png")
 plt.close()
