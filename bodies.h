da09d1d18dabc827e7a03d5ad0ee308f0c229a83
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-20 20:17:01 -0500

    Update bodies.h

--- bodies.h
+++ bodies.h
@@ -47,17 +47,10 @@ struct Cuerpo
     double rad = 0.0;
     //Masa
     double mass = 0.0;
-    //Posicion
-    Vec3D R;
-    //Velocidad
-    Vec3D V;
-    //Aceleracion auxiliar
-    Vec3D Aaux;
-    //Fuerza
-    Vec3D F;
-    //Vector energia (cenetica, potencial, mecanica)
-    Vec3D E;
-
+    //Energia
+    double E = 0.0;
+    //Posicion, Velocidad, Fuerza
+    Vec3D R, V, F;
     //Constructor condiciones iniciales
     Cuerpo(double x0, double y0, double z0, double Vx0, double Vy0, double Vz0, double m){
         this -> mass = m;
@@ -73,16 +66,8 @@ struct Cuerpo
     void start_leap_frog(const double & dt){
         V = V + F*(dt/(2*mass));
     }
-
     void leap_frog(const double & dt){
         V = V + F*(dt/(mass));
         R = R + V*dt;
     }
-
-    //Verlet algorithm
-    void verlet(const double & dt){
-        R = R + V*dt + Aaux*(dt*dt*0.5);
-        V = V + (Aaux+(F/mass))*(dt*0.5);
-    }
-
 };
