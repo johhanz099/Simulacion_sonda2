0f5a0a654c1ed0c911a0e2226f36d224ecc6e783
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-20 20:14:24 -0500

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -7,11 +7,11 @@ typedef std::vector<Cuerpo> cuerpos; //Nombre para vector
 
 //Constantes globales
 const double G = 8.2214E-7; //Constantes G
-const double T = 85; //Tiempo
+const double T = 80; //Tiempo
 const double DT = 0.0001; //Intervalo de tiempo
 const int NSTEPS = int(T/DT); //Pasos
 
 // function declaratons
-void uptade(cuerpos & Nbody);
+void force(cuerpos & Nbody);
 void trans_galileo(cuerpos & Nbody);
 void print(cuerpos & Nbody);
