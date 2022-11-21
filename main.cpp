a4d6a71c61699c8068c9b9da4483fc1ca05eda9f
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-20 20:13:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,23 +6,25 @@ int main(){
     //Datos Tierra
     double mt = 1.0;
     double rt = 1.0;
-    double v0t = sqrt(G*mS*pow(rt, -3))*rt;
+    double v0t = sqrt(G*mS*pow(rt, -3))*rt; //Orbita circular w*r 
     //Datos Jupiter
     double mj = 317.81;
-    double rj = 5.205;
-    double v0j = sqrt(G*mS*pow(rj, -3))*rj;
+    double rj = 5.203;
+    double v0j = sqrt(G*mS*pow(rj, -3))*rj; //Orbita circular w*r 
     //Datos Sonda
     double ms = 4.225E-22;
-    double v0s = 0.7;
-    double r0s = rt + (4.286E-5);
+    double rs = rt + pow(mS, -2/5);
+    double v0s = sqrt((2*G*mS*rj)/(rs*(rs+rj))); //Velocidad de transicion
+    // double rs = rt + 4.25875E-5;
+    // double v0s = 0.783; //Velocidad de transicion
 
     //---(x0,y0,z0,Vx0,Vy0,Vz0,m)
     Cuerpo Sol(0,0,0,0,0,0,mS);
     Cuerpo Tierra(rt,0,0,0,v0t,0,mt);
     Cuerpo Jupiter(rj,0,0,0,v0j,0,mj);
-    Cuerpo Sonda(r0s,0,0,0,v0s,0, ms);
+    Cuerpo Sonda(rs,0,0,0,v0s,0, ms);
 
-    cuerpos N = {Sonda, Sol, Jupiter, Tierra};
+    cuerpos N = {Sonda, Tierra, Jupiter, Sol};
     //Fuerza inicial
     force(N);
     Sol.start_leap_frog(DT);
